4b98cfd9e2f7ffffa842fe2bd8852b875f1bc2a0
maksimilz <maksimilz@mail.ru>
2024-06-14 09:52:37 +0300

    soun2

--- sounds.js
+++ sounds.js
@@ -3,18 +3,40 @@ const destroySound = new Audio('sound/Destroying_the_monst.mp3');
 const goldSound = new Audio('sound/The_sound_of_gold_co.mp3');
 const upgradeSound = new Audio('sound/unlocking_Upgrades.mp3');
 
+
+clickSound.volume = 0.5;
+destroySound.volume = 0.5;
+goldSound.volume = 0.5;
+upgradeSound.volume = 0.5;
+
 function playClickSound() {
-    clickSound.play();
+    clickSound.play().then(() => {
+        console.log('Click sound played successfully.');
+    }).catch(error => {
+        console.error('Error playing click sound:', error);
+    });
 }
 
 function playDestroySound() {
-    destroySound.play();
+    destroySound.play().then(() => {
+        console.log('Destroy sound played successfully.');
+    }).catch(error => {
+        console.error('Error playing destroy sound:', error);
+    });
 }
 
 function playGoldSound() {
-    goldSound.play();
+    goldSound.play().then(() => {
+        console.log('Gold sound played successfully.');
+    }).catch(error => {
+        console.error('Error playing gold sound:', error);
+    });
 }
 
 function playUpgradeSound() {
-    upgradeSound.play();
+    upgradeSound.play().then(() => {
+        console.log('Upgrade sound played successfully.');
+    }).catch(error => {
+        console.error('Error playing upgrade sound:', error);
+    });
 }
